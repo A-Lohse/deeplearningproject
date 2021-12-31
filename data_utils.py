55e25b6fb933ab9108b0a1dbd97599a826ad40b1
Matias Piqueras <matiaspiqueras.1994@gmail.com>
2021-12-31 21:45:36 +0100

    updated models

--- data_utils.py
+++ data_utils.py
@@ -3,7 +3,8 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 import pytorch_lightning as pl
 from typing import Optional
-
+from sklearn.utils.class_weight import compute_class_weight
+from torch.utils.data.sampler import WeightedRandomSampler
 
 def straitified_train_validation_split(embeddings, meta,targets, val_size=0.2):
     """
@@ -12,9 +13,8 @@ def straitified_train_validation_split(embeddings, meta,targets, val_size=0.2):
     # Get stratified split of data
     train_idx, val_idx = train_test_split(range(targets.shape[0]),
                                           test_size=val_size, 
-                                          random_state=3, 
+                                          random_state=1234, 
                                           stratify=targets)
-    
     return {
             'train':(embeddings[train_idx], 
                     meta[train_idx], 
@@ -24,14 +24,15 @@ def straitified_train_validation_split(embeddings, meta,targets, val_size=0.2):
                     targets[val_idx])
             }
 
-def dataloader(embeddings, meta, targets, batch_size=32,
-                num_workers=4, pin_memory=True):
+def dataloader(embeddings, meta, targets, batch_size=16,
+               num_workers=4, pin_memory=True, sampler=None):
     """
     Create dataloader for PyTorch.
     """
     return torch.utils.data.DataLoader(
         dataset=BillNetDataset(embeddings, meta, targets),
         batch_size=batch_size,
+        sampler = sampler,
         num_workers=num_workers,
         pin_memory=pin_memory)
 
@@ -55,31 +56,41 @@ class BillNetDataset(torch.utils.data.Dataset):
         return self.embeddings.shape[0]
 
 class BillNetDataModule(pl.LightningDataModule):
-    """
     
-    """
-    def __init__(self, batch_size=32,
+    def __init__(self, batch_size=16,
                        data_path='data/processed/'):
         super().__init__()
         self.batch_size = batch_size
         self.data_path = data_path
+        
 
     def setup(self):
-        #Read tensors for training and val
+        # read tensors for training and val
         bert_train = torch.load(self.data_path + 'bert_train_103-114.pt')
         meta_train = torch.load(self.data_path + 'meta_train_103-114.pt')
         targets_train = torch.load(self.data_path + 'labels_train_103-114.pt')
-        # Split training and val
+        
+        #get weights and define sampler
+        self.weights = self.compute_sampling_weights(targets_train)
+        #self.sampler = WeightedRandomSampler(weights, len(weights))                     
+        # split training and val
         splits = straitified_train_validation_split(bert_train, meta_train, targets_train)
         self.train, self.val = splits['train'], splits['val']
-        # read test
+        # read tensors for test
         self.test = (torch.load(self.data_path + 'bert_test_115.pt'), 
-                    torch.load(self.data_path + 'meta_test_115.pt'), 
-                    torch.load(self.data_path + 'labels_test_115.pt'))
+                     torch.load(self.data_path + 'meta_test_115.pt'), 
+                     torch.load(self.data_path + 'labels_test_115.pt'))
+
+    def compute_sampling_weights(self, train_targets):
+        """Calculate class weights for sampler"""
+        class_weights = compute_class_weight('balanced', 
+                                             classes=np.unique(train_targets.numpy()),
+                                             y=train_targets.numpy())
+        return torch.tensor(class_weights, dtype=torch.float)
    
     def train_dataloader(self):
         return dataloader(*self.train, batch_size=self.batch_size)
-
+                           
     def val_dataloader(self):
         return dataloader(*self.val, batch_size=self.batch_size)
 
--- run_models.py
+++ run_models.py
@@ -1,37 +1,115 @@
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
+from pytorch_lightning import Callback
 import pytorch_lightning as pl
-from models_lightning import BillNet_CNN, BillNet_FNN
+from models import BillNet_CNN, BillNet_FNN
 from data_utils import BillNetDataModule
+import pandas as pd
+from collections import defaultdict
+import copy 
+import warnings
+warnings.filterwarnings("ignore")
+
+class MetricsCallback(Callback):
+    """PyTorch Lightning metric callback."""
+    def __init__(self):
+        super().__init__()
+        self.metrics = []
+
+    def on_validation_epoch_end(self, trainer, pl_module):
+        each_me = copy.deepcopy(trainer.callback_metrics)
+        self.metrics.append(each_me)
+
+
+def parse_metrics(metrics:dict)->pd.DataFrame:
+    """
+    Helper to parse training/validation metrics from log
+    """
+    # remove metrics from sanity check
+    results  = defaultdict(list)
+    for res in metrics[1:]:
+        #val metrics
+        results['val_loss'].append(float(res['val_loss'].cpu()))
+        results['val_acc'].append(float(res['val_acc'].cpu()))
+        results['val_f1'].append(float(res['val_f1'].cpu()))
+        results['val_precision'].append(float(res['val_precision'].cpu()))
+        results['val_recall'].append(float(res['val_recall'].cpu()))
+        results['val_prauc'].append(float(res['val_prauc'].cpu()))
+        results['val_rocauc'].append(float(res['val_rocauc'].cpu()))
+        #train metrics
+        results['train_loss'].append(float(res['train_loss'].cpu()))
+        results['train_acc'].append(float(res['train_acc'].cpu()))
+        results['train_f1'].append(float(res['train_f1'].cpu()))
+        results['train_precision'].append(float(res['train_precision'].cpu()))
+        results['train_recall'].append(float(res['train_recall'].cpu()))
+        results['train_prauc'].append(float(res['train_prauc'].cpu()))
+        results['train_rocauc'].append(float(res['train_rocauc'].cpu()))
+    return pd.DataFrame(results)
 
 
 def run_models():
     #Set the seed
     pl.seed_everything(1234)
-    #Load data module
+    #Load data module and extract class weights
     dm = BillNetDataModule()
     dm.setup()
-   
-    #TODO: change monitoring to F1 score?
-    early_stop_callback = EarlyStopping(monitor="val_loss", 
-                                        min_delta=0.00, 
-                                        patience=3, 
-                                        verbose=False, 
-                                        mode="max")
-    trainer = pl.Trainer(callbacks=[early_stop_callback])
-    #Define models to run
-    models = [
-              BillNet_CNN(include_meta=True),
-              BillNet_FNN(avg_emb=False, include_meta=True),
-              BillNet_FNN(avg_emb=True, include_meta=True)
-              ]
+    class_weights = dm.weights 
+    #Define the models to run
+    models = {
+              #CNN models
+              'CNN':BillNet_CNN(include_meta=False, 
+                                class_weights=class_weights),
+              'CNN inc. meta':BillNet_CNN(include_meta=True, 
+                                          class_weights=class_weights),
+              #FNN models flattened sentence embeddings
+              'FNN':BillNet_FNN(avg_emb=False, include_meta=False, 
+                                class_weights=class_weights),
+              'FNN inc. meta':BillNet_FNN(avg_emb=False, include_meta=True,
+                                          class_weights=class_weights),
+              #FNN models avg. sentence embeddings
+              'FNN avg':BillNet_FNN(avg_emb=True, include_meta=False,
+                                    class_weights=class_weights),
+              'FNN avg inc. meta':BillNet_FNN(avg_emb=True, include_meta=True,
+                                              class_weights=class_weights)
+              }
+ 
     #-------------------------#
     #train and test the models#
     #-------------------------#
-    for model in models:
-        #Fit model
+    model_train_metrics = dict()
+    test_metrics = dict()
+    for model_name, model in models.items():
+        print('-'*66)
+        print(f'Training: {model_name}')
+        print('-'*66)
+        metrics_cb = MetricsCallback()
+        checkpoint_cb = ModelCheckpoint(monitor='val_prauc',
+                                        mode='max',
+                                        save_top_k=1,
+                                        dirpath='trained_models/',
+                                        filename=model_name+"-{epoch:02d}-{val_loss:.2f}")
+        early_stop_cb = EarlyStopping(monitor="val_prauc",
+                                      patience=5,  
+                                      mode="max")
+        #setup trainer
+        trainer = pl.Trainer(callbacks=[early_stop_cb, metrics_cb,
+                                        checkpoint_cb], gpus=1,
+                                        num_sanity_val_steps=0)
+        #Fit model and find best lr
+        #trainer.tune(model, train_dataloader=dm.train_dataloader())
         trainer.fit(model, datamodule=dm)
-        #TODO
+
+        # save model metrics
+        model_train_metrics[model_name] = parse_metrics(metrics_cb.metrics)
         #Test the model
-        
+        trainer.test(datamodule=dm, ckpt_path='best')
+        test_metrics[model_name] = trainer.logged_metrics
+        #Save the model    
+
+    print('Done!')
+    pd.DataFrame(test_metrics).to_pickle('data/results/test_metrics.pkl')
+    pd.to_pickle(model_train_metrics, 'data/results/train_val_losses.pkl')
+
+
 if __name__ == '__main__':
     run_models()
