676c48b64e953342e23b886b9d86193a564d6e45
Matias Piqueras <matiaspiqueras.1994@gmail.com>
2022-01-01 18:30:57 +0100

    Update compile_metadata.py

--- compile_metadata.py
+++ compile_metadata.py
@@ -1,5 +1,11 @@
+"""
+This scripts is used to process the metadata feed to our models
+"""
+
+
 import pandas as pd
 import numpy as np
+import torch
 
 df114 = pd.read_csv('data/raw/bills93-114.csv', sep=';', encoding='latin-1')
 df115 = pd.read_csv('data/raw/bills115-116.csv')
@@ -28,6 +34,23 @@ metadata_df = bill_status_df.merge(metadata_df, on='bill_id', how='left')
 metadata_df.drop_duplicates(subset=['bill_id'], inplace=True)
 newids = metadata_df['bill_id'].tolist()
 print(f'Missing set of ids: {set(all_ids)-set(newids)}')
-metadata_df.to_csv('data/processed/bill_metadata.csv')
-print(f'Saving to data/processed/bill_metadata.csv')
+
+#select explanatory variables and get dummies
+keepcols = ['bill_id', 'status', 'cong', 'Cosponsr', 'Majority', 'Party']
+metadata_df = metadata_df[keepcols]
+metadata_df[['republican', 'independent']] = pd.get_dummies(metadata_df['Party'], drop_first=True)
+
+#override keepcols for final
+keepcols = ['Cosponsr', 'Majority', 'republican', 'independent']
+train_meta = metadata_df.loc[metadata_df['cong'] != 115][keepcols]
+test_meta = metadata_df.loc[metadata_df['cong'] == 115][keepcols]
+
+#convert to tensors
+train_meta = torch.tensor(train_meta.to_numpy()).nan_to_num()
+test_meta = torch.tensor(test_meta.to_numpy()).nan_to_num()
+
+#save tensors
+torch.save(train_meta, 'data/processed/meta_train_103-114.pt')
+torch.save(test_meta, 'data/processed/meta_test_115.pt')
+
 
