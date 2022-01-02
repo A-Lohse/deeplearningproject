a55a30af70f2e5b73d12725e624d20069635b5a6
August Lohse <tnv664@ku.dk>
2022-01-02 21:57:23 +0100

    small update

--- src/prepare_data/make_predictions.py
+++ src/prepare_data/make_predictions.py
@@ -27,7 +27,7 @@ for m in os.listdir(model_path):
     else:    
         model_dict[m] = {}    
         model_dict[m]['val'] = {}
-git         model_dict[m]['test'] = {}
+        model_dict[m]['test'] = {}
         
         #set up the dataloader and model
         if 'CNN' in m:
