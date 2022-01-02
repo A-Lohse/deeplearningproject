7021f494907c2d88738037b35a4081696771761d
Matias Piqueras <matiaspiqueras.1994@gmail.com>
2022-01-02 21:59:23 +0100

    Update generate_metadata.py

--- src/prepare_data/generate_metadata.py
+++ src/prepare_data/generate_metadata.py
@@ -3,7 +3,6 @@ This scripts is used to process the metadata feed to our models
 data comes from the congressional Bill project: http://congressionalbills.org/
 """
 
-
 import pandas as pd
 import numpy as np
 import torch
@@ -37,17 +36,17 @@ newids = metadata_df['bill_id'].tolist()
 print(f'Missing set of ids: {set(all_ids)-set(newids)}')
 
 #select explanatory variables and get dummies
-keepcols = ['bill_id', 'status', 'cong', 'Cosponsr', 'Majority', 'Party']
+keepcols = ['bill_id', 'status', 'cong', 'Cosponsr', 'Majority', 'Party', 'Gender']
 metadata_df = metadata_df[keepcols]
 metadata_df[['republican', 'independent']] = pd.get_dummies(metadata_df['Party'], drop_first=True)
 metadata_df.drop(columns=['Party'], inplace=True)
 
-metadata_df = metadata_df.rename(columns={'Cosponsr':'cosponsors', 'Majority':'majority'})
+metadata_df = metadata_df.rename(columns={'Cosponsr':'cosponsors', 'Majority':'majority', 'Gender':'gender'})
 #save metadata
 metadata_df.to_csv('data/processed/bills_metadata.csv', index=False)
 
 #override keepcols for final
-keepcols = ['cosponsors', 'majority', 'republican', 'independent', 'cong']
+keepcols = ['cosponsors', 'majority', 'republican', 'independent', 'cong', 'gender']
 metadata_df = metadata_df[keepcols]
 train_meta = metadata_df.loc[metadata_df['cong'] != 115].drop(columns=['cong'])
 test_meta = metadata_df.loc[metadata_df['cong'] == 115].drop(columns=['cong'])
